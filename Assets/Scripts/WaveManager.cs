d7a81bb017a01079a2e2d1c2860e20a5f6af045f
Kai <kai520958@gmail.com>
2023-02-03 17:46:59 -0600

    Wave timings

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -8,28 +8,31 @@ public class WaveManager : MonoBehaviour
     public GameObject waveObject;
     public int size;
     public int growthRate;
+    public float downTime;
+    public float minWait;
+    public float maxWait;
     public UnityEvent<int> waveStarted;
     private int activeCount;
-    private int currentWave = 1;
+    private int currentWave = -1;
 
-    private void StartWave()
+    private void Start()
     {
+        StartCoroutine(startWave());
+    }
+
+    private IEnumerator startWave()
+    {
+        waveStarted?.Invoke(++currentWave);
+        yield return new WaitForSeconds(downTime);
+
         activeCount = size + currentWave * growthRate;
         for (int i = 0; i < activeCount; i++)
         {
+            yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+
             var location = transform.GetChild(Random.Range(0, transform.childCount));
             var obj = Instantiate(waveObject, location.position, location.rotation);
             obj.GetComponent<WaveObject>().manager = this;
-        }
-
-        waveStarted?.Invoke(++currentWave);
-    }
-
-    private void Update()
-    {
-        if (activeCount <= 0)
-        {
-            StartWave();
         }
     }
 
@@ -39,6 +42,6 @@ public class WaveManager : MonoBehaviour
     public void WaveObjectDestroyed()
     {
         if (--activeCount <= 0)
-            StartWave();
+            StartCoroutine(startWave());
     }
 }
